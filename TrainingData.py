1efd7ca752a614a0a5e72e7f308450b4a804d363
psxnr4 <psxnr4@nottingham.ac.uk>
2025-08-18 15:54:19 +0100

    Update TrainingData.py - adding context

--- TrainingData.py
+++ TrainingData.py
@@ -3,6 +3,7 @@
 import torch
 from transformers import AdamW, BertTokenizer, BertForMaskedLM, set_seed, DataCollatorForLanguageModeling #BERT mlm
 from torch.utils.data import ConcatDataset
+import os
 import string
 from sklearn.model_selection import train_test_split
 
@@ -11,6 +12,10 @@ tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 # Get BERT masked language model from Hugging Face
 mlm_model = BertForMaskedLM.from_pretrained('bert-base-uncased')
 mlm_model.eval()
+
+directory_root = 'C:/Users/nat/OneDrive - The University of Nottingham/Documents/Dissertation/P69 - Aphasia Project/4. Analysis and Results'
+control_directory = directory_root + '/Capilouto-Umbrella-Gem/Cleaned-Flo'
+aphasia_directory = directory_root + '/Aphasia Training Data/Repaired-Flo'
 
 # Define training dataset
 class TextDataset(torch.utils.data.Dataset):
@@ -25,19 +30,27 @@ class TextDataset(torch.utils.data.Dataset):
         item = {key: val[idx] for key, val in self.encodings.items()}
 
         if self.target_ids is not None:
-            item['target_ids'] = torch.tensor(self.target_ids[idx])
-
+            if idx < len(self.target_ids):
+                item['target_ids'] = torch.tensor(self.target_ids[idx])
+            else:
+                raise IndexError(f"Index {idx} out of range for target_ids of length {len(self.target_ids)}")
         return item
+
 
 
 def get_training_data():
     # Create dataset from the control transcripts
-    control_file_path = '../control_training_combined_output.cex'
-    control_dataset = dataset_from_file(control_file_path)
+    #control_file_path = '../control_training_combined_output.cex'
+    #control_dataset = dataset_from_file(control_file_path)
 
     # Create dataset from the repaired aphasia transcripts
-    aphasia_file_path = '../aphasia_training_combined_output.cex'
-    aphasia_dataset = dataset_from_file(aphasia_file_path)
+    #aphasia_file_path = '../aphasia_training_combined_output.cex'
+    #aphasia_dataset = dataset_from_file(aphasia_file_path)
+
+    print("Getting control data")
+    control_dataset = dataset_from_directory(control_directory)
+    print("Getting aphasia data")
+    aphasia_dataset = dataset_from_directory(aphasia_directory)
 
     # Concat datasets to prevent resetting the optimisers between training sets
     combined_dataset = ConcatDataset([control_dataset, aphasia_dataset])
@@ -47,16 +60,48 @@ def get_training_data():
 
 
 
+def dataset_from_directory(dir_path):
+    # Create 2d array of all data
+    all_data = []
+    # Get all .cex files in the directory
+    for filename in os.listdir(dir_path):
+        if filename.endswith('.cex'):
+            file_path = os.path.join(dir_path, filename)
+            with open(file_path, 'r', encoding='utf-8') as f:
+               data = f.read()
+            # Separate each utterance to be processed individually
+            data = data.split('\n')
+            # Add context to each utterance
+            data_w_context = add_context(data,12)
+            all_data.extend(data_w_context)
+
+    print(type(all_data), len(all_data))
+    print(all_data[:5])
+
+    # -- Tokenise data
+    inputs = tokenizer(
+        all_data, max_length=512, truncation=True, padding=True, return_tensors=None
+    )  # keys: input_ids, token_type_ids, attention_mask
+
+    # Create dataset to define how to load and batch the tokenized data
+    dataset = TextDataset(inputs)
+
+    print(f" -- Created Dataset size: {len(dataset)}")
+
+    return dataset
+
+
+
+
 # Read in training data from the given path, tokenise and create dataset
 def dataset_from_file(file_path):
-    # Read test data
+    # Read test data as each line a seperate item in a list
     with open(file_path, 'r', encoding='utf-8') as f:
-        lines = [line.rstrip().rstrip('.') for line in f] # remove trailing periods
-    content = ' '.join(lines) # remove line breaks
+        train_data = [line.rstrip().rstrip('.') for line in f] # remove trailing periods
 
-    # Convert to list, splitting at each transcript break delimiter
-    train_data = content.split('|')
-    #print(train_data)
+    # Convert to list of transcripts, splitting at each transcript break delimiter
+    # content = ' '.join(lines) # remove line breaks
+    #train_data = content.split('|')
 
     # -- Tokenise data
     inputs = tokenizer(
@@ -83,9 +128,39 @@ def load_dataset(dataset):
         shuffle=True,
         collate_fn=data_collator
     )
-
     return dataloader
 
 
+# Add previous and trailing lines to each utterance to introduce context
+# Build an adaptive context window around the masked line of at least min number of words
+# text: array of strings
+def add_context(text, min_size):
+    text_w_context = []
+    for index in range(len(text)):
+        line_w_context = text[index].split()
+        length = len(line_w_context)
+        buffer = 1
+
+        while length < min_size:
+            prev_line = text[index - buffer].split() if index - buffer >= 0 else []
+            next_line = text[index + buffer].split() if index + buffer < len(text) else []
+            buffer += 1
+
+            # Add context around the line
+            line_w_context = prev_line + ['[SEP]'] + line_w_context + ['[SEP]'] + next_line
+            length = len(line_w_context)
+
+        # Join the combined lines into a single string and add to array
+        final_line = ' '.join(line_w_context)
+        text_w_context.append(final_line)
+
+        print('\n')
+        for l in text_w_context:
+            print(l)
+    return text_w_context
 
 
+
+
+
+get_training_data()
